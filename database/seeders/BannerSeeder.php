d4f36a32691ce8e39a74c03726be2bafa38451c6
Khairul Anam <eenkanamz@gmail.com>
2022-07-24 20:00:04 +0700

    Membuat seeder banner

--- database/seeders/BannerSeeder.php
+++ database/seeders/BannerSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\banner;
 
 class BannerSeeder extends Seeder
 {
@@ -14,6 +15,10 @@ class BannerSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $data = [
+            'foto' => 'Ini iklan makanan'
+        ];
+
+        Banner::create($data);
     }
 }
