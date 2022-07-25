af3cc9a133fe720db65ff4243b64ac749ac64a4d
Khairul Anam <eenkanamz@gmail.com>
2022-07-26 06:01:35 +0700

    Menambahkan model, controller dan migration untuk menu makanan

---
+++ app/Http/Controllers/MakananController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Makanan;
+use Illuminate\Http\Request;
+
+class MakananController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Makanan  $makanan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Makanan $makanan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Makanan  $makanan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Makanan $makanan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Makanan  $makanan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Makanan $makanan)
+    {
+        //
+    }
+}
