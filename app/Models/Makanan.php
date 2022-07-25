47ba830537af7e538b5d7011144809b9b06eba97
Khairul Anam <eenkanamz@gmail.com>
2022-07-26 06:09:33 +0700

    Menambahkan kode kedalam model makanan

--- app/Models/Makanan.php
+++ app/Models/Makanan.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Makanan extends Model
 {
-    //
+    use HasFactory;
+    protected $table = 'makanans';
+    const CREATED_AT = null;
+    const UPDATED_AT = null;
+    protected $guarded = [];
 }
