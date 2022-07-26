50654115917ecd4a48c3f6e5d156fc2f4d866427
Khairul Anam <eenkanamz@gmail.com>
2022-07-26 07:34:41 +0700

    Menambahkan kode kedalam method di controller makanan

--- app/Http/Controllers/MakananController.php
+++ app/Http/Controllers/MakananController.php
@@ -7,58 +7,61 @@ use Illuminate\Http\Request;
 
 class MakananController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        $makanans = Makanan::all();
+        return response()->json($makanans);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        // Validasi
+        $this->validate($request, [
+            'nama' => 'required',
+            'harga' => 'required',
+            'gambar' => 'required'
+        ]);
+
+        $makanan = new Makanan();
+
+        // Input teks
+        $makanan->nama = $request->input('nama');
+        $makanan->harga = $request->input('harga');
+
+        // Unggah gambar
+        if ($request->hasFile('gambar')) {
+            $file = $request->file('gambar');
+            $allowedFileExtensions = ['jpg', 'png'];
+            $extension = $file->getClientOriginalExtension();
+            $check = in_array($extension, $allowedFileExtensions);
+
+            if ($check) {
+                $name = time() . $file->getClientOriginalName();
+                $file->move('assets/images', $name);
+                $makanan->gambar = url('assets/images/' . $name);
+            }
+        }
+
+        $makanan->save();
+
+        return response()->json($makanan);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Makanan  $makanan
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Makanan $makanan)
+    public function show($id)
     {
-        //
+        $makanan = Makanan::where('id', $id)->get();
+        return response()->json($makanan);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Makanan  $makanan
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Makanan $makanan)
+    public function update(Request $request, $id)
     {
-        //
+        Makanan::where('id', $id)->update($request->all());
+        return response()->json('Data Sudah di Update!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Makanan  $makanan
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Makanan $makanan)
+    public function destroy($id)
     {
-        //
+        Makanan::where('id', $id)->delete();
+        return response()->json('Data berhasil dihapus!');
     }
 }
