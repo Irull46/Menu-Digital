a2c72c2c8b6270246cc032b2887833be3303faf2
Khairul Anam <eenkanamz@gmail.com>
2022-07-26 08:31:40 +0700

    Menambahkan model, controller dan migration untuk menu minuman

---
+++ app/Http/Controllers/MinumanController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Minuman;
+use Illuminate\Http\Request;
+
+class MinumanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Minuman  $minuman
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Minuman $minuman)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Minuman  $minuman
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Minuman $minuman)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Minuman  $minuman
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Minuman $minuman)
+    {
+        //
+    }
+}
