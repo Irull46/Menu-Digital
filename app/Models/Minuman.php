dc8eb64c933beb42897696a8aafd0f8d4ea71351
Khairul Anam <eenkanamz@gmail.com>
2022-07-26 08:32:33 +0700

    Menambahkan kode kedalam model minuman

--- app/Models/Minuman.php
+++ app/Models/Minuman.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Minuman extends Model
 {
-    //
+    use HasFactory;
+    protected $table = 'minumans';
+    const CREATED_AT = null;
+    const UPDATED_AT = null;
+    protected $guarded = [];
 }
